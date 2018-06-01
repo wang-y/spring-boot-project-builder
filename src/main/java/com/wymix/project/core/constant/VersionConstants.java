96f6e930213d595b4fe5e06d5586db74e56dbc64
wang-y <wy90314@gmail.com>
2018-06-01 16:39:39 +0800

    update version

--- src/main/java/com/wymix/project/core/constant/VersionConstants.java
+++ src/main/java/com/wymix/project/core/constant/VersionConstants.java
@@ -4,8 +4,8 @@ public final class VersionConstants {
 
     public final static String SPRING_BOOT_VERSION="2.0.2.RELEASE";
 
-    public final static String DRUID_VERSION="1.1.9";
+    public final static String DRUID_VERSION="1.1.10";
 
-    public final static String FASTJSON_VERSION="1.2.46";
+    public final static String FASTJSON_VERSION="1.2.47";
 
 }

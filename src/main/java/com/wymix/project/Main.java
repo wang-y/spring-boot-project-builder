a0409a709924f84bb0b4d5c4663f66f3460fe8b8
wang_yang <wang_yang@travelsky.com>
2019-12-20 16:35:38 +0800

    optimize: 优化引导信息

--- src/main/java/com/wymix/project/Main.java
+++ src/main/java/com/wymix/project/Main.java
@@ -23,8 +23,8 @@ public class Main {
         System.out.println("https://github.com/wang-y/spring-boot-project-builder");
         System.out.println("-----------------------------------------------------");
         System.out.println("                                           (欢迎fork)");
-        buildProject();
-//        build();
+//        buildProject();
+        build();
     }
 
 
